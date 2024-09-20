e2b10610972e4df0b66d9ab3ad2d8f5c576f4d78
James Buoncristiani <jbuoncri@gmail.com>
2024-09-20 12:24:19 -0700

    Added shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Button } from '@/components/ui/button';
+
 function App() {
   return (
     <>
       <div className='flex flex-col items-center justify-center f'>
         <h1 className='text-3xl font-bold'>Pokemart Northwest</h1>
         <p>Coming soon</p>
+        <Button>Click me</Button>
       </div>
     </>
   );
