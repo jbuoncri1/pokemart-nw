0cdb9ef1ab11ae65518cba6c3a520f45517d4dcd
James Buoncristiani <jbuoncri@gmail.com>
2024-09-19 22:05:17 -0700

    added boilerplate and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className='flex flex-col items-center justify-center f'>
+        <h1 className='text-3xl font-bold'>Pokemart Northwest</h1>
+        <p>Coming soon</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
